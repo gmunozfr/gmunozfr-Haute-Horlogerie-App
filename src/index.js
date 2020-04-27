5e42edc4a8d9bc5bfc542c473e0d06ce191a135c
gmunozfr <fragiga81+github@gmail.com>
2020-04-27 00:45:30 +0000

    My first commit - defining variables & connecting to server

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// <!-- The following code contains a similar approach in regards to the following tutorial:https://www.youtube.com/watch?v=WV7DYx2M-Nc -->
+// <!-- This is the github account of the author: https://github.com/marktellez/marktellez.github.io/commits/master?after=fd3b6cd992d64bce29136c963b5531b628b42677+34 -->
+
+
+'use strict'; //this is going to force it into a strict mode
+
+require('dotenv').config() //is going to load a dotenv file
+var express = require('express')
+var app = express()//calling the function required from express
+var port = process.env.PORT || 3000 //defining the port that is going to be used
+var bodyParser = require ('body-parser')// is going to be used by the express server to parse the incoming body of the request
+var mongoose = require('mongoose')//being able to handle the relationship between the models and mongodb server
+var cors = require("cors")
+var http = require("http")
+
+var routes = require('./routes')
+var cors =  require('./cors')//it handles the security for cross site scripting
+
+mongoose.Promise = global.Promise //this is the global namespace
+mongoose.connect('mongodb://localhost/timepieces-server', {useNewUrlParser: true });//connecting to localhost 
+mongoose.set('useFindAndModify', false)//to get rid of the warning while compiling
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');//establishing connection 
+});
+
+
+
+app.use(bodyParser.urlencoded({ extended : true}))
+app.use(bodyParser.json())//here Json is going to be used
+
+// cors(app)// here the app will be called. Is the cors call where we pass the app to the cors file
+// routes(app) //passing the app to the routes
+
+
+app.listen(port, function(err){
+    console.log("The Fabulous Timepieces App is Listening on Port: " + port)
+});
+
