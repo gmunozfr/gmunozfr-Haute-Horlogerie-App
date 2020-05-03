f584432b998cdcbb5c90048a9010067860d0eabc
gmunozfr <fragiga81+github@gmail.com>
2020-05-03 20:34:30 +0000

    finishing adding references in relation to the API

--- server/src/controllers/log_entries.js
+++ server/src/controllers/log_entries.js
@@ -1,5 +1,6 @@
 // <!-- The following code contains a similar approach in regards to the following tutorial:https://www.youtube.com/watch?v=WV7DYx2M-Nc -->
 // <!-- This is the github account of the author: https://github.com/marktellez/marktellez.github.io/commits/master?after=fd3b6cd992d64bce29136c963b5531b628b42677+34 -->
+// <!-- Another reference in regards to the author is: https://github.com/marktellez/marktellez.github.io -->
 
 
 'use strict'
